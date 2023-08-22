4226a0ccb0bee1c715cf59f6309196a81adb5391
JSantoro <jeremie.santoro@heig-vd.ch>
2023-08-22 14:11:52 +0200

    ajout de fb hosting avec gh actions

--- g2g/lib/main.dart
+++ g2g/lib/main.dart
@@ -39,7 +39,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Dorian le chien'),
     );
   }
 }
