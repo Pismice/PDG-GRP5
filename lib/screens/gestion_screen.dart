bae4854def257f278e0b891c30dd3afe86dedb0b
Théo Coutaudier <theo.coutaudier@heig-vd.ch>
2023-08-28 17:32:57 +0200

    [WIP] Dynamic list with no relevant data

--- lib/screens/gestion_screen.dart
+++ lib/screens/gestion_screen.dart
@@ -1,11 +1,31 @@
 import 'package:flutter/material.dart';
 
 class GestionScreen extends StatelessWidget {
-  const GestionScreen({super.key});
+  final List<String> items = List<String>.generate(100, (i) => 'Item $i');
+  GestionScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // Ici choisir si on veut s'occuper des workouts/séances/exercices
-    return const Placeholder();
+    const title = 'Long List';
+
+    return MaterialApp(
+      title: title,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(title),
+        ),
+        body: ListView.builder(
+          itemCount: items.length,
+          prototypeItem: ListTile(
+            title: Text(items.first),
+          ),
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(items[index]),
+            );
+          },
+        ),
+      ),
+    );
   }
 }
