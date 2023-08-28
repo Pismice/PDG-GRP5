2af36d8295b429ec165f2178f1c3db07fc6b669b
Théo Coutaudier <theo.coutaudier@heig-vd.ch>
2023-08-28 16:17:46 +0200

    debut

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,6 +5,58 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: Scaffold(
+          body: /*Stack(children: [
+          Container(
+            color: Colors.blue,
+            padding: const EdgeInsets.all(5.0),
+            child: const Text(
+              'data',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),*/
+              ListView(
+        padding: const EdgeInsets.all(8),
+        children: <Widget>[
+          Container(
+              padding: const EdgeInsets.all(16),
+              margin: const EdgeInsets.all(24),
+              height: 150,
+              color: Colors.amber[100],
+              child: ListView(
+                padding: const EdgeInsets.all(8),
+                children: [
+                  const Text('TITRE WORKOUT'),
+                  Container(
+                    margin: const EdgeInsets.only(top: 24),
+                    padding: const EdgeInsets.all(8),
+                    color: Colors.amber[300],
+                    child: const Text('SEANCE 1'),
+                  )
+                ],
+              )),
+          Container(
+              padding: const EdgeInsets.all(16),
+              margin: const EdgeInsets.all(24),
+              height: 150,
+              color: Colors.amber[100],
+              child: ListView(
+                padding: const EdgeInsets.all(8),
+                children: [
+                  const Text('TITRE WORKOUT 2'),
+                  Container(
+                    margin: const EdgeInsets.only(top: 24),
+                    padding: const EdgeInsets.all(8),
+                    color: Colors.amber[300],
+                    child: const Text('SEANCE 1'),
+                  )
+                ],
+              )),
+        ],
+      )),
+    );
   }
 }
