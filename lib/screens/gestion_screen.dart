91c109e7b2858b1290846db693d408b3122c0a53
Théo Coutaudier <theo.coutaudier@heig-vd.ch>
2023-08-28 21:20:07 +0200

    add search bar

--- lib/screens/gestion_screen.dart
+++ lib/screens/gestion_screen.dart
@@ -1,29 +1,86 @@
 import 'package:flutter/material.dart';
 
+void main() => runApp(GestionScreen());
+
 class GestionScreen extends StatelessWidget {
-  final List<String> items = List<String>.generate(100, (i) => 'Item $i');
-  GestionScreen({super.key});
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Workout',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Mes Activités'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  TextEditingController editingController = TextEditingController();
+
+  final duplicateItems = List<String>.generate(10000, (i) => "Item $i");
+  var items = <String>[];
+
+  @override
+  void initState() {
+    items = duplicateItems;
+    super.initState();
+  }
+
+  void filterSearchResults(String query) {
+    setState(() {
+      items = duplicateItems
+          .where((item) => item.toLowerCase().contains(query.toLowerCase()))
+          .toList();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    const title = 'Long List';
-
-    return MaterialApp(
-      title: title,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text(title),
-        ),
-        body: ListView.builder(
-          itemCount: items.length,
-          prototypeItem: ListTile(
-            title: Text(items.first),
-          ),
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text(items[index]),
-            );
-          },
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: TextField(
+                onChanged: (value) {
+                  filterSearchResults(value);
+                },
+                controller: editingController,
+                decoration: const InputDecoration(
+                    labelText: "Recherche",
+                    hintText: "Mon Workout",
+                    prefixIcon: Icon(Icons.search),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)))),
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                shrinkWrap: true,
+                itemCount: items.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(
+                        '${items[index]}'), /* Obligatoir en cas de liste vide*/
+                  );
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
