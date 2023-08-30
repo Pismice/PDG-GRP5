1bbc250f9ee0a6ff53cb4159f76dd4551af04ec6
Dorian Gillioz <gilliozdorian@gmail.com>
2023-08-30 08:47:15 +0200

    [WIP] update action + gestion image

--- lib/api/firebase_exercice.dart
+++ lib/api/firebase_exercice.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 class GetExercice extends StatelessWidget {
   final String documentId;
@@ -27,12 +28,9 @@ class GetExercice extends StatelessWidget {
         if (snapshot.connectionState == ConnectionState.done) {
           Map<String, dynamic> data =
               snapshot.data!.data() as Map<String, dynamic>;
+
           return Column(children: [
-            Image.network(
-              data['img'],
-              height: 100,
-              width: 100,
-            ),
+            image(data),
             Text("Nom de l'exercice: ${data['name']}")
           ]);
         }
@@ -41,4 +39,39 @@ class GetExercice extends StatelessWidget {
       },
     );
   }
+
+  Widget image(Map<String, dynamic> data) {
+    return FutureBuilder(
+      future: FirebaseStorage.instance
+          .refFromURL('gs://hongym-4cb68.appspot.com')
+          .child("img/exercises/${data['img']}")
+          .getDownloadURL(),
+      builder: (context, snapshot) {
+        return Image.network(
+          snapshot.data.toString(),
+          height: 100,
+          width: 100,
+        );
+      },
+    );
+  }
+}
+
+CollectionReference exercises =
+    FirebaseFirestore.instance.collection('exercise');
+
+Future<void> addExercise(String name) async {
+  return exercises
+      .add({'name': name, 'img': ''})
+      .then((_) => print("exercice ajouté"))
+      .catchError(
+          (error) => print("L'exercice n'a pas pu être ajouté : $error"));
+}
+
+Future<void> updateExercise(String docId, String newName) async {
+  return exercises
+      .doc(docId)
+      .update({'name': newName})
+      .then((_) => print("modification effectuée"))
+      .catchError((error) => print('Erreur : $error'));
 }
