07aeec2abf2fa813803ed593c185e1eadb3e748d
Dorian Gillioz <gilliozdorian@gmail.com>
2023-08-30 10:04:01 +0200

    grrrrrr

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'navigation_bar.dart';
-import 'screens/user_connection/google_sign_in_screen.dart';
 
 void main() async {
   // Initialisation de Firebase
