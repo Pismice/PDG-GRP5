85642da0fa1ddcf7208a6d8444a47157ac8b0755
Dorian Gillioz <gilliozdorian@gmail.com>
2023-09-02 17:42:58 +0200

    [WIP] Tests sur les actions de séances (y a juste le getAll qui merde)

--- test/firestore_exercise_test.dart
+++ test/firestore_exercise_test.dart
@@ -2,9 +2,27 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:g2g/model/exercice.dart';
 import 'package:g2g/model/session.dart';
+import 'package:g2g/model/workout.dart';
 import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
 
 final instance = FakeFirebaseFirestore();
+
+Future<DocumentReference<Map<String, dynamic>>> getReference(
+    Exercise ex) async {
+  return await exercises
+      .where('name', isEqualTo: ex.name)
+      .where('img', isEqualTo: ex.img)
+      .limit(1)
+      .get()
+      .then((value) => exercises.doc(value.docs[0].id));
+}
+
+Future<void> addUser() async {
+  await instance.collection('user').add({
+    'authid': 'nimportequoi',
+    'name': 'Bob',
+  });
+}
 
 Future<void> addExercice(Exercise e) async {
   await instance.collection('exercise').add({
@@ -17,26 +35,32 @@ Future<DocumentSnapshot<Object?>> getExercise(String documentId) async {
   return exercises.doc(documentId).get();
 }
 
+Future<void> updateExercise(String docId, String newName) async {
+  try {
+    await exercises.doc(docId).update({'name': newName});
+  } on Exception catch (e) {
+    throw Exception("Erreur lors de la modification : $e");
+  }
+}
+
+Future<void> deleteExercise(String docId) async {
+  try {
+    await exercises.doc(docId).delete();
+  } on Exception catch (e) {
+    throw Exception("Erreur lors de la suppressions : $e");
+  }
+}
+
 final users = instance.collection('user');
 final exercises = instance.collection('exercise');
 
 void main() async {
-  addExercice(Exercise(name: "my exercice"));
-
-  final snapshot = await users.get();
-
-  setUpAll(() async {});
+  Exercise e = Exercise(name: "cool pompe", img: "pompe.png");
 
   test("addExercise", () async {
-    Exercise e = Exercise(name: "cool pompe", img: "pompe.png");
     await addExercice(e);
 
-    final exRef = await exercises
-        .where('name', isEqualTo: e.name)
-        .where('img', isEqualTo: e.img)
-        .limit(1)
-        .get()
-        .then((value) => exercises.doc(value.docs[0].id));
+    final exRef = await getReference(e);
 
     final doc = await getExercise(exRef.id);
 
@@ -46,20 +70,31 @@ void main() async {
   });
 
   test("updateExercise", () async {
-    Exercise e = Exercise(name: "cool pompe", img: "pompe.png");
     await addExercice(e);
 
-    final exRef = await exercises
-        .where('name', isEqualTo: e.name)
-        .where('img', isEqualTo: e.img)
-        .limit(1)
-        .get()
-        .then((value) => exercises.doc(value.docs[0].id));
+    final exRef = await getReference(e);
+
+    updateExercise(exRef.id, "super cool pompe");
 
     final doc = await getExercise(exRef.id);
 
     Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
-    expect(e.name, data['name']);
-    expect(e.img, data['img']);
+
+    expect(data['name'], "super cool pompe");
+  });
+
+  test("deleteExercise", () async {
+    await addExercice(e);
+
+    final exRef = await getReference(e);
+
+    var doc = await getExercise(exRef.id);
+
+    expect(doc.exists, true);
+
+    deleteExercise(exRef.id);
+
+    doc = await getExercise(exRef.id);
+    expect(doc.exists, false);
   });
 }
---
+++ test/test_models/session.dart
@@ -0,0 +1,107 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
+
+final instance = FakeFirebaseFirestore();
+
+class Session {
+  String? name;
+  String? user;
+  int? duration;
+  List<SessionExercises>? exercises;
+
+  Session({this.name, this.user, this.duration, this.exercises});
+
+  Session.fromJson(Map<String, dynamic> json) {
+    name = json['name'];
+    user = json['user'].id;
+    duration = json['duration'];
+    if (json['exercises'] != null) {
+      exercises = <SessionExercises>[];
+      json['exercises']
+          .forEach((e) => exercises!.add(SessionExercises.fromJson(e)));
+    }
+  }
+
+  factory Session.fromFirestore(DocumentSnapshot<Map<String, dynamic>> snapshot,
+      SnapshotOptions? options) {
+    final data = snapshot.data();
+
+    return Session();
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    if (name != null) data['name'] = name;
+
+    if (user != null) {
+      data['user'] = instance.doc("user/$user");
+    }
+
+    if (duration != null) data['duration'] = duration;
+    if (exercises != null) {
+      data['exercises'] = exercises!.map((e) => e.toJson()).toList();
+    }
+    return data;
+  }
+
+  Map<String, dynamic> toFirestore() {
+    return {
+      if (name != null) "name": name,
+      if (user != null) "user": instance.doc("user/$user"),
+      if (duration != null) "duration": duration,
+      if (exercises != null)
+        "exercises": exercises!.map((e) => e.toFirestore()).toList(),
+    };
+  }
+}
+
+class SessionExercises {
+  String? id;
+  int? repetition;
+  int? set;
+  int? weight;
+  int? duration;
+
+  SessionExercises(
+      {this.id, this.repetition, this.set, this.weight, this.duration});
+
+  SessionExercises.fromJson(Map<String, dynamic> json) {
+    id = json['id'].id;
+    repetition = json['repetition'];
+    set = json['set'];
+    weight = json['weight'];
+    duration = json['duration'];
+  }
+
+  factory SessionExercises.fromFirestore(
+      DocumentSnapshot<Map<String, dynamic>> snapshot,
+      SnapshotOptions? options) {
+    final data = snapshot.data();
+    return SessionExercises(
+      id: data?['id'].id,
+      repetition: data?['repetition'],
+      set: data?['set'],
+      weight: data?['weight'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = instance.doc("session/$id");
+    if (repetition != null) data['repetition'] = repetition;
+    if (set != null) data['set'] = set;
+    if (weight != null) data['weight'] = weight;
+    if (duration != null) data['duration'] = duration;
+    return data;
+  }
+
+  Map<String, dynamic> toFirestore() {
+    return {
+      "id": instance.doc("exercise/$id"),
+      if (repetition != null) "repetition": repetition,
+      if (set != null) "set": set,
+      if (weight != null) "weight": weight,
+      if (duration != null) "duration": duration,
+    };
+  }
+}
