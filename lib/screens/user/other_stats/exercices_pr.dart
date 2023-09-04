8ed798f2c9f76a2a7b9aaf09a86276e496ec704c
pismice <pismice15@gmail.com>
2023-09-04 20:17:46 +0200

    [WIP] Page des PR + recup des exo depuis firestore

--- lib/screens/user/other_stats/exercices_pr.dart
+++ lib/screens/user/other_stats/exercices_pr.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-
-import '../../../model/exercice.dart';
+import 'package:g2g/api/firebase_exercise.dart';
+import 'package:g2g/model/exercise.dart';
 
 class ExercicesPr extends StatefulWidget {
   const ExercicesPr({super.key});
@@ -16,19 +16,31 @@ class _ExercicesPrState extends State<ExercicesPr> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Mes PR"),
-      ),
-      body: Column(
-        children: [
-          Row(
-            children: [
-              Text("mon exo"),
-              Text("10 kg"),
-            ],
-          )
-        ],
-      ),
-    );
+        appBar: AppBar(
+          title: const Text("Mes PR"),
+        ),
+        body: FutureBuilder(
+            future: getAllExercisesFrom(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const CircularProgressIndicator();
+              } else if (snapshot.hasError) {
+                return Text("Eroor: ${snapshot.error}");
+              } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                return const Text(
+                    "Do you first exercices in order to have PRs displayed");
+              } else {
+                final exercices = snapshot.data!;
+                return ListView.builder(
+                    itemCount: exercices.length,
+                    itemBuilder: (context, index) {
+                      final exercise = exercices[index];
+                      return ListTile(
+                        title: Text(exercise.name ?? "No name"),
+                        subtitle: Text("0 kg"),
+                      );
+                    });
+              }
+            }));
   }
 }
