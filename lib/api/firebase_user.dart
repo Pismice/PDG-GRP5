bc3d0754ee3473754312c7c9c57b73236174ff2b
obaume <oscar.baume@gmail.com>
2023-09-04 13:23:49 +0200

    ajout deleteUser

--- lib/api/firebase_user.dart
+++ lib/api/firebase_user.dart
@@ -2,6 +2,9 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:g2g/model/user.dart';
 
 final users = FirebaseFirestore.instance.collection('user');
+final exercise = FirebaseFirestore.instance.collection('exercise');
+final session = FirebaseFirestore.instance.collection('session');
+final workout = FirebaseFirestore.instance.collection('workout');
 
 Future<User> getUser(String authid) async {
   final snapshot =
@@ -9,4 +12,27 @@ Future<User> getUser(String authid) async {
 
   final data = snapshot.docs.map((e) => e.data()).first;
   return User.fromJson(data);
+}
+
+void deleteUser(String authid) async {
+  DocumentReference ref = users
+          .where('authid', isEqualTo: authid)
+          .get()
+          .then((value) => users.doc(value.docs[0].id))
+      as DocumentReference<Map<String, dynamic>>;
+  List<QueryDocumentSnapshot<Map<String, dynamic>>> docs =
+      exercise.where('user', isEqualTo: ref).get().then((value) => value.docs)
+          as List<QueryDocumentSnapshot<Map<String, dynamic>>>;
+  docs.addAll(
+      session.where('user', isEqualTo: ref).get().then((value) => value.docs)
+          as List<QueryDocumentSnapshot<Map<String, dynamic>>>);
+
+  docs.addAll(
+      workout.where('user', isEqualTo: ref).get().then((value) => value.docs)
+          as List<QueryDocumentSnapshot<Map<String, dynamic>>>);
+
+  for (var doc in docs) {
+    doc.reference.delete();
+  }
+  ref.delete();
 }
