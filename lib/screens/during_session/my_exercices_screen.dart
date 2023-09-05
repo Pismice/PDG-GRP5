843291663ccd373095a72bc580ae1313775c3162
pismice <pismice15@gmail.com>
2023-09-05 16:42:14 +0200

    [WIP] Structure quasi OK manque de quoi inserer dans la BD (backend)

--- lib/screens/during_session/my_exercices_screen.dart
+++ lib/screens/during_session/my_exercices_screen.dart
@@ -1,29 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:g2g/api/firebase_exercise.dart';
+import 'package:g2g/model/session.dart';
 import 'package:g2g/screens/during_session/my_repetition_screen.dart';
 
 class MyExercices extends StatelessWidget {
-  const MyExercices({super.key});
+  const MyExercices({super.key, required this.session});
+  final Session session;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Le nom de ma séance"),
-      ),
-      body: Column(
-        children: [
-          const Text(
-              "ici il y aura la liste des exercices de la séance avec le widget à theo"),
-          ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const MyRepetition()));
-              },
-              child: const Text("un exercice au hasard"))
-        ],
-      ),
-    );
+        appBar: AppBar(
+          title: Text(session.name!),
+        ),
+        body: ListView.builder(
+          itemCount: session.exercises!.length,
+          itemBuilder: (context, index) {
+            return FutureBuilder(
+                future: getExercise(session.exercises![index].id!),
+                builder: (context, exoBase) {
+                  return Row(
+                    children: [
+                      Image(
+                          image: NetworkImage(exoBase.data!.img ??
+                              "https://cdn-icons-png.flaticon.com/512/1053/1053916.png")),
+                      Column(
+                        children: [
+                          Text(exoBase.data!.name!),
+                          Row(
+                            children: [
+                              Text(
+                                  "${session.exercises![index].set.toString()} x ${session.exercises![index].repetition.toString()}")
+                            ],
+                          )
+                        ],
+                      ),
+                    ],
+                  );
+                });
+          },
+        ));
   }
 }
--- lib/screens/during_session/my_repetition_screen.dart
+++ lib/screens/during_session/my_repetition_screen.dart
@@ -2,9 +2,15 @@ import 'dart:math';
 
 import 'package:confetti/confetti.dart';
 import 'package:flutter/material.dart';
+import 'package:g2g/api/firebase_session.dart';
+import 'package:g2g/model/exercise.dart';
+import 'package:g2g/model/session.dart';
 
 class MyRepetition extends StatefulWidget {
-  const MyRepetition({super.key});
+  const MyRepetition(
+      {super.key, required this.exercise, required this.exoBase});
+  final SessionExercises exercise;
+  final Exercise exoBase;
 
   @override
   State<MyRepetition> createState() => _MyRepetitionState();
@@ -13,6 +19,7 @@ class MyRepetition extends StatefulWidget {
 class _MyRepetitionState extends State<MyRepetition> {
   bool isPlaying = false;
   final controller = ConfettiController();
+  final _formKey = GlobalKey<FormState>();
 
   @override
   void initState() {
@@ -27,43 +34,76 @@ class _MyRepetitionState extends State<MyRepetition> {
 
   @override
   Widget build(BuildContext context) {
+    TextEditingController ctrlNbReps = TextEditingController();
+    TextEditingController ctrlWeight = TextEditingController();
     return Stack(
       alignment: Alignment.center,
       children: [
         Scaffold(
           appBar: AppBar(
-            title: const Text("Nom de l'exercice"),
+            title: Text(widget.exoBase.name!),
           ),
-          body: Column(
-            children: [
-              const Text("Série 2 sur 3"),
-              const Row(
-                children: [
-                  Text("Nombre de répétitions"),
-                  Expanded(child: TextField()),
-                ],
-              ),
-              const Row(
-                children: [
-                  Text("Poids (kg)"),
-                  Expanded(child: TextField()),
-                ],
-              ),
-              const Image(
-                width: 100.0,
-                fit: BoxFit.fitWidth,
-                image: NetworkImage(
-                    'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
-              ),
-              ElevatedButton(
-                  onPressed: () {
-                    controller.play();
-                    Future.delayed(const Duration(milliseconds: 500), () {
-                      controller.stop();
-                    });
-                  },
-                  child: const Text("Valider ma série"))
-            ],
+          body: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                const Text("Série 2 sur 3"),
+                Row(
+                  children: [
+                    const Text("Nombre de répétitions"),
+                    Expanded(
+                        child: TextFormField(
+                      keyboardType: TextInputType.number,
+                      controller: ctrlNbReps,
+                      validator: (value) {
+                        if (int.parse(value!) < 0) {
+                          return "Must be positive";
+                        }
+                        return null;
+                      },
+                    )),
+                  ],
+                ),
+                Row(
+                  children: [
+                    const Text("Poids (kg)"),
+                    Expanded(
+                        child: TextFormField(
+                      keyboardType: TextInputType.number,
+                      controller: ctrlWeight,
+                      validator: (value) {
+                        if (int.parse(value!) < 0) {
+                          return "Must be positive";
+                        }
+                        return null;
+                      },
+                    )),
+                  ],
+                ),
+                const Image(
+                  width: 100.0,
+                  fit: BoxFit.fitWidth,
+                  image: NetworkImage(
+                      'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
+                ),
+                ElevatedButton(
+                    onPressed: () async {
+                      if (_formKey.currentState!.validate()) {
+                        if (await getWeightPR(widget.exoBase.uid!) <
+                            int.parse(ctrlWeight.text)) {
+                              // PR battu !
+                          controller.play();
+                          Future.delayed(const Duration(milliseconds: 500), () {
+                            controller.stop();
+                          });
+                        }
+                        // Inserer dans la BD l exo effectue
+                        
+                      }
+                    },
+                    child: const Text("Valider ma série"))
+              ],
+            ),
           ),
         ),
         ConfettiWidget(
