df6df783452e9a5ae464e2008356989a2f0da4d8
Théo Coutaudier <theo.coutaudier@heig-vd.ch>
2023-09-06 11:15:16 +0200

    correcto oscaro

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -19,79 +19,83 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Workout'),
+          ),
           body: FutureBuilder(
-        future: getAllWorkoutsFrom(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          }
-          if (snapshot.hasError) {
-            return Text("Error ${snapshot.error.toString()}");
-          }
-          if (snapshot.connectionState == ConnectionState.done &&
-              snapshot.hasData) {
-            return ListView.builder(
-                shrinkWrap: true,
-                itemCount: snapshot.data!.length,
-                itemBuilder: (context, i) {
-                  return Container(
-                      padding: const EdgeInsets.all(15),
-                      child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                              foregroundColor: Colors.black,
-                              padding:
-                                  const EdgeInsets.fromLTRB(20, 10, 20, 10),
-                              backgroundColor: Colors.white),
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => MyWorkoutInfoPage(
-                                      snapshot.data![i].uid as String)),
-                            );
-                          },
-                          child: Column(children: <Widget>[
-                            Align(
-                                alignment: Alignment.bottomLeft,
-                                child: Container(
-                                    padding: const EdgeInsets.all(10),
-                                    child: Text(
-                                        snapshot.data![i].name as String))),
-                            ListView.builder(
-                                shrinkWrap: true,
-                                itemCount: snapshot.data![i].sessions!.length,
-                                itemBuilder: (context, j) {
-                                  return Container(
-                                      padding: const EdgeInsets.all(8),
-                                      child: FutureBuilder(
-                                        builder: ((context, snapshot) {
-                                          if (snapshot.connectionState ==
-                                                  ConnectionState.done &&
-                                              snapshot.hasData) {
-                                            return ElevatedButton(
-                                                onPressed: () {
-                                                  /*Changer de pages where la séance en cours*/
-                                                },
-                                                child: Text(snapshot.data!
-                                                    .data()!['name']));
-                                          }
-                                          return const Center(
-                                              child:
-                                                  CircularProgressIndicator());
-                                        }),
-                                        future: FirebaseFirestore.instance
-                                            .collection('session')
-                                            .doc(snapshot
-                                                .data![i].sessions![j].id)
-                                            .get(),
-                                      ));
-                                })
-                          ])));
-                });
-          }
-          return const Center(child: CircularProgressIndicator());
-        },
-      )),
+            future: getAllWorkoutsFrom(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              }
+              if (snapshot.hasError) {
+                return Text("Error ${snapshot.error.toString()}");
+              }
+              if (snapshot.connectionState == ConnectionState.done &&
+                  snapshot.hasData) {
+                return ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: snapshot.data!.length,
+                    itemBuilder: (context, i) {
+                      return Container(
+                          padding: const EdgeInsets.all(15),
+                          child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                  foregroundColor: Colors.black,
+                                  padding:
+                                      const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                                  backgroundColor: Colors.white),
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => MyWorkoutInfoPage(
+                                          snapshot.data![i].uid as String)),
+                                );
+                              },
+                              child: Column(children: <Widget>[
+                                Align(
+                                    alignment: Alignment.bottomLeft,
+                                    child: Container(
+                                        padding: const EdgeInsets.all(10),
+                                        child: Text(
+                                            snapshot.data![i].name as String))),
+                                ListView.builder(
+                                    shrinkWrap: true,
+                                    itemCount:
+                                        snapshot.data![i].sessions!.length,
+                                    itemBuilder: (context, j) {
+                                      return Container(
+                                          padding: const EdgeInsets.all(8),
+                                          child: FutureBuilder(
+                                            builder: ((context, snapshot) {
+                                              if (snapshot.connectionState ==
+                                                      ConnectionState.done &&
+                                                  snapshot.hasData) {
+                                                return ElevatedButton(
+                                                    onPressed: () {
+                                                      /*Changer de pages where la séance en cours*/
+                                                    },
+                                                    child: Text(snapshot.data!
+                                                        .data()!['name']));
+                                              }
+                                              return const Center(
+                                                  child:
+                                                      CircularProgressIndicator());
+                                            }),
+                                            future: FirebaseFirestore.instance
+                                                .collection('session')
+                                                .doc(snapshot
+                                                    .data![i].sessions![j].id)
+                                                .get(),
+                                          ));
+                                    })
+                              ])));
+                    });
+              }
+              return const Center(child: CircularProgressIndicator());
+            },
+          )),
     );
   }
 }
