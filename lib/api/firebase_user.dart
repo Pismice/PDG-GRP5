66baee8232946cbbaaa6e9d607b19651f0af515a
obaume <oscar.baume@gmail.com>
2023-09-06 23:22:51 +0200

    [WIP] Update firebase_user.dart

--- lib/api/firebase_user.dart
+++ lib/api/firebase_user.dart
@@ -7,6 +7,8 @@ final exercise = FirebaseFirestore.instance.collection('exercise');
 final session = FirebaseFirestore.instance.collection('session');
 final workout = FirebaseFirestore.instance.collection('workout');
 
+/// Fonction qui retourne l'utilisateur avec [authid]
+/// comme identifiant d'authentification
 Future<User> getUser(String authid) async {
   final snapshot =
       await users.where('authid', isEqualTo: authid).limit(1).get();
@@ -17,32 +19,37 @@ Future<User> getUser(String authid) async {
   return user;
 }
 
+/// Fonction qui supprime l'utilisateur avec [authid]
+/// comme identifiant d'authentification
 Future<void> deleteUser(String authid) async {
   DocumentReference ref = await users
       .where('authid', isEqualTo: authid)
       .get()
       .then((value) => users.doc(value.docs[0].id));
+  // recupère les document exercice qui ont été crée par l'utilisateur
   List<QueryDocumentSnapshot<Map<String, dynamic>>> docs = await exercise
       .where('user', isEqualTo: ref)
       .get()
       .then((value) => value.docs);
-
+  // récupère les document séance qui ont été crée par l'utilisateur
   docs.addAll(await session
       .where('user', isEqualTo: ref)
       .get()
       .then((value) => value.docs));
-
+  // récupère les document qui ont été crée par l'utilisateur
   docs.addAll(await workout
       .where('user', isEqualTo: ref)
       .get()
       .then((value) => value.docs));
-
+  // supprime tous les doc qui ont été crée par l'utilisateur
   for (var doc in docs) {
     doc.reference.delete();
   }
+  // supprime l'utilisateur
   ref.delete();
 }
 
+/// Fonction qui mets à jour un [user]
 void updateUser(User user) async {
   User storeUser = User.fromFirestore(
       await users
@@ -56,6 +63,7 @@ void updateUser(User user) async {
   }
 }
 
+/// Fonction qui ajout un nouveau utilisateur venant de google
 Future<void> addNewGoogleUserToFirestore(UserCredential user) {
   return FirebaseFirestore.instance.collection('user').add(<String, String>{
     'authid': user.user!.uid,
@@ -64,6 +72,7 @@ Future<void> addNewGoogleUserToFirestore(UserCredential user) {
   });
 }
 
+/// Fonction qui ajoute un nouveau user depuis un mail
 Future<void> addNewEmailUserToFirestore(UserCredential user,
     [String? username]) {
   return FirebaseFirestore.instance.collection('user').add(<String, String>{
