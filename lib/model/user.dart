b661d9d9c8aff06e28b639dd8e1cbf858a26b981
obaume <oscar.baume@gmail.com>
2023-09-06 23:59:32 +0200

    [WIP]Update user.dart

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+/// Classe qui représente un utilisateur
 class User {
+  // id du document de la bdd
   String? uid;
   String? authId;
   String? name;
@@ -8,6 +10,7 @@ class User {
 
   User({this.uid, this.authId, this.name, this.profilepicture});
 
+  /// Fonction qui crée un user à partir du documentSnapshot
   factory User.fromFirestore(DocumentSnapshot<Map<String, dynamic>> snapshot,
       SnapshotOptions? options) {
     final data = snapshot.data();
@@ -19,12 +22,14 @@ class User {
         profilepicture: data?['profilepicture']);
   }
 
+  /// Constructeur à partir d'un json
   User.fromJson(Map<String, dynamic> json) {
     authId = json['authid'];
     name = json['name'];
     profilepicture = json['profilepicture'];
   }
 
+  /// Fonction qui retourne l'utilisateur format de la bdd
   Map<String, dynamic> toFirestore() {
     return {
       "authid": authId,
